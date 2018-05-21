80b5534df8b744c47b8005e2d463bf6cec3a1a59
MaxHalford <maxhalford25@gmail.com>
2018-05-21 13:43:34 +0200

    Do appveyor.yml

--- ci_scripts/travis/install.sh
+++ ci_scripts/travis/install.sh
@@ -11,14 +11,12 @@ cd download
 echo "Cached in $HOME/download :"
 ls -l
 echo
-
 if [[ ! -f miniconda.sh ]]
    then
    wget http://repo.continuum.io/miniconda/Miniconda-3.6.0-Linux-x86_64.sh \
        -O miniconda.sh
    fi
 chmod +x miniconda.sh && ./miniconda.sh -b
-
 cd ..
 export PATH=/home/travis/miniconda/bin:$PATH
 conda update --yes conda
