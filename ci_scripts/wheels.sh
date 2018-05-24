d3ffa68ebf1d3eb96cb7a60d4bec45d807e37e41
MaxHalford <maxhalford25@gmail.com>
2018-05-24 21:47:25 +0200

    Fix travis wheels

--- ci_scripts/wheels.sh
+++ ci_scripts/wheels.sh
@@ -1,21 +1,24 @@
 # Use the miniconda installer for faster download / install of conda
 # itself
-cd
-mkdir -p download
-cd download
-echo "Cached in $HOME/download :"
-ls -l
-echo
-if [[ ! -f miniconda.sh ]]
-   then
-   wget http://repo.continuum.io/miniconda/Miniconda-3.6.0-Linux-x86_64.sh \
-       -O miniconda.sh
-   fi
-chmod +x miniconda.sh && ./miniconda.sh -b
-cd ..
-export PATH=/home/travis/miniconda/bin:$PATH
-conda update --yes conda
-conda create -n wheelenv --yes python=3.5
-source activate wheelenv
-pip install cibuildwheel==0.8.0
-cibuildwheel --output-dir wheelhouse
+if [[ $TRAVIS_TAG ]];
+    then
+    cd
+    mkdir -p download
+    cd download
+    echo "Cached in $HOME/download :"
+    ls -l
+    echo
+    if [[ ! -f miniconda.sh ]]
+       then
+       wget http://repo.continuum.io/miniconda/Miniconda-3.6.0-Linux-x86_64.sh \
+           -O miniconda.sh
+       fi
+    chmod +x miniconda.sh && ./miniconda.sh -b
+    cd ..
+    export PATH=/home/travis/miniconda/bin:$PATH
+    conda update --yes conda
+    conda create -n wheelenv --yes python=3.5
+    source activate wheelenv
+    pip install cibuildwheel==0.8.0
+    cibuildwheel --output-dir wheelhouse
+    fi
