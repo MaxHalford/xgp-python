7de247303e1d46c8e927bc3356e1c0a90c73db3b
Max Halford <maxhalford25@gmail.com>
2018-09-09 20:50:04 +0200

    Fix pypi

--- ci_scripts/pypi.sh
+++ ci_scripts/pypi.sh
@@ -25,9 +25,8 @@ pip install setuptools-golang
 docker run --rm --volume /home/travis/gopath/src/github.com/MaxHalford/xgp-python/dist:/dist:rw --user 2000:2000 quay.io/pypa/manylinux1_x86_64:latest bash -o pipefail -euxc 'cd /tmp
 curl https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz --silent --location | tar -xz
 for py in cp35-cp35m cp36-cp36m; do
-    "/opt/python/$py/bin/pip" wheel --no-deps --wheel-dir /tmp /dist/*.tar.gz
+    "/opt/python/$py/bin/pip" wheel --no-deps --wheel-dir /tmp /dist
 done
-ls *.whl | xargs -n1 --verbose auditwheel repair --wheel-dir /dist
 ls -al /dist
 '
 
